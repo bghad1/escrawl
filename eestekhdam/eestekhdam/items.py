ca7fc7d8619e03996cbd16874b335d49f90da8b0
bghad1 <bghad1@users.noreply.github.com>
2021-07-23 13:09:48 +0430

    Delete eestekhdam directory

--- eestekhdam/eestekhdam/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class EestekhdamItem(scrapy.Item):
-    position_url = scrapy.Field()
-    position = scrapy.Field()
-    phone = scrapy.Field()
-    email = scrapy.Field()
-    pass
